f7252301575f71e27c55106a4959bc974dc3947c
Roman Liutikov <roman01la@romanliutikov.com>
2015-05-07 23:57:32 +0300

    read/write to localStorage

--- src/app.jsx
+++ src/app.jsx
@@ -11,6 +11,7 @@ import { flip } from './lib/functions';
 
 import page from 'page';
 import _ from 'mori';
+import mt from 'mori-transit';
 
 let addRoute = _.partial(flip(page), _.comp(
 
@@ -18,12 +19,19 @@ let addRoute = _.partial(flip(page), _.comp(
     _.partial((prop, obj) => obj[prop], 'path')
 ));
 
+function getInitialState() {
+
+    let localState = localStorage.getItem('todo-state');
+    return localState ? mt.decode(localState) : _.toClj(initialState);
+}
+
 addRoute('/');
 addRoute('/all');
 addRoute('/active');
 addRoute('/completed');
 page(ToDoItemsStore.getCurrentPage());
 
-let AtomicToDoApp = atomComponent(ToDoApp, initialState);
+let AtomicToDoApp = atomComponent(ToDoApp, getInitialState(),
+        state => localStorage.setItem('todo-state', mt.encode(state)));
 
 React.render(<AtomicToDoApp />, document.body);
